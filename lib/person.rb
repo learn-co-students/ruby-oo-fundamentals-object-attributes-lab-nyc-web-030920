1a55825a8c799dd077e65b4769d7fd33d7d19d79
Asilver-jpg <asilver7493@gmail.com>
2020-03-10 11:08:12 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+   
+    
+    def name
+    @name
+    end
+    
+    def name=(new_name)
+    @name = new_name
+    end
+    
+    def job
+        @job
+        end
+        
+    def job=(new_name)
+    @job = new_name
+     end
+    
+    end
