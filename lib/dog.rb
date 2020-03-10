e2aab508b939dcc175aa40d32411ef0e8f37934d
github username <github email address>
2020-03-10 11:49:55 -0400

    commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
         @name = dog_name 
     end 
     def name 
-        @dog_name 
+        @name 
     end 
     def breed=(dog_breed) 
         @breed = dog_breed 
     end 
     def breed 
-        @dog_breed 
+        @breed 
     end 
 end 
