2fa14485a6f983ea95df173fcc4a464855e94191
github username <github email address>
2020-03-10 11:51:04 -0400

    commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 def Person 
-    def name=(person_name) 
-        @name = person_name 
+    def name=(name) 
+        @name = name 
     end 
     def name 
         @name 
     end 
-    def job=(person_job) 
-        @job = person_job 
+    def job=(job) 
+        @job = job 
     end 
     def job 
         @job 
